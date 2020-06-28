ce9f7e45687f3996a8c98e673a87d7628fa4e3fa
crutchbandicoot <tsheposet@hotmail.com>
2020-06-28 09:25:53 +0200

    added routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,39 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello world from the express app!'
-    });
+//Database connection
+mongoose.connect(
+  "mongodb://localhost:27017/coffe_shop",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to the database");
+    }
+  }
+);
+// Middleware
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Routes
+const productRoute = require("./api/routes/products");
+const orderRoute = require("./api/routes/orders");
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello from the express app!",
+  });
 });
+
+app.use("/products", productRoute);
+app.use("/orders", orderRoute);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 
 
-port = process.env.PORT || 8080;
+port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
